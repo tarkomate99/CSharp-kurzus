8e0959d30058b5e9a2ff75132cba73403d57d934
Irinyi Kabinet Magyar Nyelvű Felhasználó <valaki@pc21920-3.inf.u-szeged.hu>
2022-03-10 14:19:30 +0100

    ora4.4

---
+++ 4ora/Test/TestPelda.Tests/RandomPrimeProviderTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace TestPelda.Tests
+{
+
+    internal class TestRandomProvider : IRandomProvider
+    {
+        public int GetNumber(int minimum, int maximum)
+        {
+            return 3;
+        }
+    }
+
+    [TestFixture]
+    internal class RandomPrimeProviderTests
+    {
+
+        public void EnsureThat_Random_PrimeProvideWorks()
+        {
+
+            RandomPrimeProvider provider = new RandomPrimeProvider(new TestRandomProvider());
+            int result = provider.GetNumber(0, 100);
+            Assert.AreEqual(3, result);
+
+        }
+
+
+    }
+}
--- 4ora/Test/TestPelda/RandomPrimeProvider.cs
+++ 4ora/Test/TestPelda/RandomPrimeProvider.cs
@@ -6,8 +6,42 @@ using System.Threading.Tasks;
 
 namespace TestPelda
 {
-    internal class RandomPrimeProvider
+    public interface IRandomProvider
     {
+        int GetNumber(int minimum, int maxium);
+    }
 
+    public class RandomNUmberProvider : IRandomProvider
+    {
+        private Random _rng = new Random();
+
+        public int GetNumber(int minimum, int maxium)
+        {
+            return _rng.Next(minimum, maxium);
+        }
+    }
+
+    public class RandomPrimeProvider
+    {
+        private readonly IRandomProvider _randomProvider;
+
+        public RandomPrimeProvider(IRandomProvider randomProvider)
+        {
+            _randomProvider = randomProvider;
+        }
+
+        public int GetNumber(int minimum, int maximum) {
+            bool isPrime = false;
+            do
+            {
+                int candidate = _randomProvider.GetNumber(minimum, maximum);
+                isPrime = PrimeFinder.isPrime(candidate);
+                if (isPrime)
+                    return candidate;
+            }
+            while (!isPrime);
+
+            throw new InvalidOperationException("This shouldn't happen...");
+        }
     }
 }
