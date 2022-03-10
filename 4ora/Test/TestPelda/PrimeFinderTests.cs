e9876110eb14346adbbc059970e1bcacc7b590c8
Irinyi Kabinet Magyar Nyelvű Felhasználó <valaki@pc21920-3.inf.u-szeged.hu>
2022-03-10 13:25:57 +0100

    added

---
+++ 4ora/Test/TestPelda/PrimeFinderTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestPelda
+{
+    [TestFixture]
+    internal class PrimeFinderTests
+    {
+
+        public void EnsureThat_PrimeFinder_isPrime_ReturnsTrue_WhenPrime()
+        {
+            
+        }
+    }
+}
