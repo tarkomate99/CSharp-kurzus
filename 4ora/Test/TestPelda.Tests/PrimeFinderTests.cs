86f6fb5df52249d4a75684e9bd2cdae68054f0ec
Irinyi Kabinet Magyar Nyelvű Felhasználó <valaki@pc21920-3.inf.u-szeged.hu>
2022-03-10 13:43:38 +0100

    ora4

---
+++ 4ora/Test/TestPelda.Tests/PrimeFinderTests.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace TestPelda
+{
+    [TestFixture]
+    internal class PrimeFinderTests
+    {
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(8)]
+        [TestCase(12)]
+        [TestCase(13)]
+
+        public void EnsureThat_PrimeFinder_isPrime_ReturnsTrue_WhenPrime(int input)
+        {
+
+            //Arrange + Act
+            Assert.IsTrue(PrimeFinder.isPrime(input));
+        }
+    }
+}
---
+++ 4ora/Test/TestPelda/PrimeFinder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestPelda
+{
+    public static class PrimeFinder
+    {
+        public static bool isPrime(int input) 
+        {
+            if (input == 2) return true;
+            if (input < 3) return false; 
+            for (int i = 3; i < Math.Sqrt(input); i++) 
+            {
+                if (input % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
